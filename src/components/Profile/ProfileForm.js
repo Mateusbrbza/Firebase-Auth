2ecc118587f0ad70959f703c3f5292c06cb83306
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-11-01 23:01:21 -0300

    .

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,22 +1,40 @@
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
 
+import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
   const newPasswordInputRef = useRef();
+  const authCtx = useContext(AuthContext);
 
   const submitHandler = event => {
     event.preventDefault();
 
     const enteredNewPassword = newPasswordInputRef.current.value;
+
     // could add validation here
-  }
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyC8JW3d6XXCTcV-1Wz5wkk66pMzc7UofRs', {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredNewPassword,
+        returnSecureToken: false,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      //assumindo que sempre dara certo
+      console.log("senha alterada")
+    })
+  };
 
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>Nova senha</label>
-        <input type='password' id='new-password' ref={newPasswordInputRef} />
+        <input type='password' id='new-password' minLengh="5" ref={newPasswordInputRef} />
       </div>
       <div className={classes.action}>
         <button>Mudar senha</button>
